2158fbbede04427e27524a24c665250e5497b19d
Muhammad Rizki Fonna <13516001@std.stei.itb.ac.id>
2021-11-18 13:09:15 +0700

    add test4 answer

--- BE-test/test4.js
+++ BE-test/test4.js
@@ -9,6 +9,18 @@ const numbers = [9, 6, 4, 2, 3, 5, 7, 0, 1];
 
 function result(numbers) {
   // Your Code Here
+  numbers=numbers.sort();
+  for (var i=0;i<numbers.length-1;i++)
+  {
+    if (numbers[i+1]!=numbers[i]+1)
+    {
+      return numbers[i]+1;
+    }
+  }
+  return number[numbers.length-1]+1;
+
+
+
 }
 
 console.log(result(numbers));
