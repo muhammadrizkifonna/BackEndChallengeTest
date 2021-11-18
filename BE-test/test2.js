288a6285c728d0b3cb3cf9de4857c87abf090931
Muhammad Rizki Fonna <13516001@std.stei.itb.ac.id>
2021-11-18 13:04:05 +0700

    add test2 answer

--- BE-test/test2.js
+++ BE-test/test2.js
@@ -5,13 +5,28 @@
  * Expected Result:
  * [1, 2, 3, 4, 5]
  */
-const data = [1, 4, 2, 3, 5, 3, 2, 4];
+ const data = [1, 4, 2, 3, 5, 3, 2, 4];
 
-function result(data) {
-  // Your Code Here
-  
-
-
-}
-
-console.log(result(data));
+ function result(data) {
+   // Your Code Here
+   const res=[];
+   let map_number= new Map();
+   // console.log("A");
+   for (var i=0;i<data.length;i++)
+   {
+     // console.log(data[i])
+     if (!map_number.has(data[i]))
+     {
+       map_number.set(data[i], 1);
+       res.push(data[i]);
+       
+     }
+   }
+   return res.sort();
+ 
+ 
+ 
+ }
+ 
+ console.log(result(data));
+ 
